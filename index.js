c1df97a9c29e707402e8581c8fa34eb5452dad26
dbyrd <methyl8@comcast.net>
2022-09-22 17:29:46 -0500

    udpate index and generateMarkdown js files

--- index.js
+++ index.js
@@ -1,13 +1,89 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer")
+const fs = require("fs")
+const genMd = require("./utils/generateMarkdown")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+      type: "input",
+      name: "projectTitle",
+      message: "What is the name of your project?"
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Enter a description of your project:"
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "Enter any installation instructions:",
+      default: "npm i"
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Enter the usage information for your project:"
+    },
+    {
+      type: "input",
+      name: "contributions",
+      message: "Enter the contribution guidelines for your project:"
+    },
+    {
+      type: "input",
+      name: "test",
+      message: "Enter the test instructions for your project:"
+    },
+    {
+      type: "list",
+      name: "license",
+      choices: ["Apache License 2.0",
+      "GNU General Public License v3.0",
+      "MIT License",
+      "BSD 'Simplified' License",
+      "BSD 'New' or 'Revised' License",
+      "Boost Software License 1.0",
+      "Creative Commons Zero v1.0 Universal",
+      "Eclipse Public License 2.0",
+      "GNU Affero General Public License v3.0",
+      "GNU General Public License v2.0",
+      "GNU Lesser General Public License v2.1",
+      "Mozilla Public License 2.0",
+      "The Unlicense"]
+    },
+    {
+      type: "input",
+      name: "profile",
+      message: "Enter your GitHub name:"
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Enter your contact email:",
+      default: "No email contact, please."
+    }
+];
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+  .prompt(
+    questions
+  )
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    fs.writeFile("./test-README.md",genMd(answers), (err) => 
+    {
+      if(err) {console.error(err)}
+    })
+  })
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,87 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license) {
+    let splitLicense = license.split(" ")
+    let noSpace = splitLicense.join("%20")
+    return `https://img.shields.io/badge/license-${noSpace}-blue.svg`
+  }
+  else {
+    return ""
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license) {
+    switch (license) {
+      case "Apache License 2.0":
+        return `[Apache](https://choosealicense.com/licenses/apache-2.0/)`
+      case "GNU General Public License v3.0":
+      case "GNU Affero General Public License v3.0":
+      case "GNU General Public License v2.0":
+      case "GNU Lesser General Public License v2.1":
+        return `[GPL](https://choosealicense.com/licenses/gpl-3.0/)`;
+      case "MIT License":
+        return `[MIT](https://choosealicense.com/licenses/mit/)`;
+      case "BSD 'Simplified' License":
+      case "BSD 'New' or 'Revised' License":
+        return `[ISC](https://choosealicense.com/licenses/isc/)`;
+      default:
+        return license;
+    }
+  }
+  else {
+    return ""
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return {
+    badge: renderLicenseBadge(license),
+    link: renderLicenseLink(license)
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  console.log("ppp");
+  let license = renderLicenseSection(data.license)
+  return `![license](${license.badge})
+  # ${data.projectTitle}
 
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  ${license.link}
+
+  ## Contributing
+  ${data.contributions}
+
+  ## Tests
+  ${data.test}
+
+  ## Questions
+  - GitHub profile: [GitHub Profile](https://${data.profile}.github.com)
+  - Email me at: ${data.email}
 `;
 }
 
